c45f9d92805b75ebd855b1444e830b78753ff768
Godfred Annum Jnr <gannumjnr@gmail.com>
2025-10-29 22:14:45 +0000

    Updated the user profile and did some of the image changes

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,8 @@
 from django.forms import ModelForm 
 from django import forms
-from django.contrib.auth.models import User
 from .models import Room
 from .models import Message
+from .models import User
 
 
 
